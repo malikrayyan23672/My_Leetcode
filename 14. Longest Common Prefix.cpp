9e78f835399341e9bffed4dfe83422401b3d5377
malikrayyan23672 <malikrayyan263@gmail.com>
2026-08-11 15:50:07 +0500

    14. Longest Common Prefix initial code added

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};
+
+int main(){
+
+    Solution s;
+
+    vector<string> strs = {"flower","flow","flight"};
+
+    string result = s.longestCommonPrefix(strs);
+
+    cout << "Result = " << result << endl;
+
+}
