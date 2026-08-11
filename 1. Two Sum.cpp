38913aedef27aadacea3c6629852ebd589407801
malikrayyan23672 <malikrayyan263@gmail.com>
2026-08-11 15:31:35 +0500

    TwoSum problem initial default code added

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
+int main(){
+
+    Solution s;
+
+    vector<int> nums = {3,2,4};
+    int target = 6;
+
+    vector<int> result = s.twoSum(nums, target);
+
+}
