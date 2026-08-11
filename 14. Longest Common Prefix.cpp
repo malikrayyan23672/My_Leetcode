c9f6000221a204f3525d4e5c63a2a25f25f72a7d
malikrayyan23672 <malikrayyan263@gmail.com>
2026-08-11 16:58:41 +0500

    14. Longest Common Prefix solution added

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -7,6 +7,39 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+
+        string result = "";
+
+        int index=0; //index of the string
+
+        while(true){
+
+            // size of string should be greater then the index variable.
+            if(strs[0].size() <= index){
+
+                return result;
+
+            }
+
+            char a = strs[0][index]; //character at index of first string in the vector
+
+            for(int i=1; i<strs.size(); i++){
+
+                if(strs[i][index] != a){
+
+                    return result;
+
+                }
+
+
+            }
+
+            result += a;
+
+            index++;
+        }
+
+        return result;
         
     }
 };
