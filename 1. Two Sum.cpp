4d0a45a0962126f68d5b05e2402a754db5630f79
malikrayyan23672 <malikrayyan263@gmail.com>
2026-08-11 15:42:38 +0500

    TwoSum problem solution added

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+
+        vector<int> result;
+
+        unordered_map<int,int> mp;
+
+        for(int i=0; i<nums.size(); i++){
+
+            int currentNumber = nums[i];
+            int t = target-currentNumber;
+
+            if(mp.find(t) != mp.end()){ //if the t exists in the unordered_map then we have found our solution
+
+                result.push_back(mp[t]);
+                result.push_back(i);
+
+            }
+
+            //if the t is not found then add the current number to the map
+            mp[currentNumber] = i;
+
+        }
+
+        return result;
+
     }
 };
 
@@ -18,5 +42,10 @@ int main(){
     int target = 6;
 
     vector<int> result = s.twoSum(nums, target);
+
+    for(auto i : result){
+
+        cout << i << " ";
+    }
 
 }
